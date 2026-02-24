85a02d6f02d8cb9e0d0ebf335237188484e4aeaf
Shikhar Bisen <shikharbisen284@gmail.com>
2026-02-24 14:06:27 +0530

    Remove unnecessary newline in app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fileUpload =require('express-fileupload');
 const cors = require('cors')
-
 const userRoute = require('./routes/user');
 const courseRoute = require('./routes/course');
 const studentRoute = require('./routes/student');
